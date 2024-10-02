5a1d02096b6fc79183351765fdb9e1fea607cce7
Michael Reisz <michael_reisz@yahoo.com>
2024-10-01 22:57:48 -0400

    Add a GlobalUsings file

---
+++ src/EconomyDataLoader/EconomyDataLoader/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using Microsoft.Extensions.Configuration;
+
+global using EconomyDataLoader.Data;
+global using EconomyDataLoader.Models.BEA;
+global using EconomyDataLoader.Models.BLS;
+global using EconomyDataLoader.Models.StockIndex;
+global using EconomyDataLoader.Utilities;
