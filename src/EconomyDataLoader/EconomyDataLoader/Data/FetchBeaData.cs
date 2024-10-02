6647e42d8dd617f56b342917b09260aab029215b
Michael Reisz <michael_reisz@yahoo.com>
2024-10-01 22:58:42 -0400

    Add a FetchBeaData class

---
+++ src/EconomyDataLoader/EconomyDataLoader/Data/FetchBeaData.cs
@@ -0,0 +1,30 @@
+﻿namespace EconomyDataLoader.Data;
+
+internal static class FetchBeaData
+{
+    public static async Task<List<BeaDataRecord>> FetchDataAsync(string registrationKey)
+    {
+        List<BeaDataRecord> results = [];
+        var client = new HttpClient();
+
+        string URL = string.Format(
+            "https://apps.bea.gov/api/data/?UserID={0}&method=GetData&datasetname=Regional&TableName=CAINC1&LineCode=3&Year=ALL&GeoFips=00000&ResultFormat=json",
+            registrationKey);
+
+        Console.WriteLine(URL);
+
+        try
+        {
+            var response = await client.GetAsync(URL);
+            var content = await response.Content.ReadAsStringAsync();
+            var beaData = JsonSerializer.Deserialize<BeaRequestResult>(content);
+            results.AddRange(beaData!.BeaApi.Results.Data);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+        return results.OrderBy(r => r.TimePeriod).ToList();
+    }
+}
